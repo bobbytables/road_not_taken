495be5575152f6aa26e37a666813b1dff00ed619
BobbyTables <robert@creativequeries.com>
2016-03-04 14:26:55 -0500

    Initial Commit.

---
+++ lib/road_not_taken.rb
@@ -0,0 +1,5 @@
+require "road_not_taken/version"
+
+module RoadNotTaken
+  # Your code goes here...
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'road_not_taken'
